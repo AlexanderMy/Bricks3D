55210c528634f5f263683030b43427426c00c64c
AlexanderMyznikov <sasha@eventsoft.com.ua>
2015-04-18 21:42:23 +0300

    Created project. Setup camera and base scripts. Created Line figure, that Fall down and recreated.

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Constants{
+	public const int X_CELLS = 7;
+	public const int Y_CELLS = 10;
+	public const int Z_CELLS = 7;
+	public const float X_CELL_SIZE = 10.0f;
+	public const float Y_CELL_SIZE = 10.0f;
+	public const float Z_CELL_SIZE = 10.0f;
+	public const float MOVE_STEP = 10.0f;
+	public const float ROTATION_STEP = 90.0f;
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public float speed = 10.0f;
+	public GameObject figureLine;
+
+	private Vector3 spawnPosition = new Vector3(35.0f, 155.0f, 35.0f);
+
+	public GameObject[,,] cubes = new GameObject[7, 10, 7];
+	// Use this for initialization
+	void Start () {
+		InstantiateFigure();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate()
+	{
+
+	}
+
+	public void InstantiateFigure()
+	{
+		GameObject gObject = (GameObject)Instantiate(figureLine, spawnPosition, Quaternion.identity);
+		FigureController figureController = gObject.GetComponent<FigureController>();
+		figureController.controller = this;
+	}
+
+	public bool canFallDown(GameObject [] cubes){
+		foreach(var cube in cubes){
+			Debug.Log(cube.transform.position);
+			Vector3 cubePosition = cube.transform.position;
+
+		}
+		return false;
+	}
+}
